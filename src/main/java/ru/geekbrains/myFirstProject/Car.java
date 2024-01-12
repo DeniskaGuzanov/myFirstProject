e814b13656e6444ace232b2e782d9057485c257e
denis <xdenis@yandex.ru>
2024-01-12 10:54:10 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/myFirstProject/Car.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.myFirstProject;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Car {
+    @Autowired
+    Engine engine;
+
+
+    public void start(){
+        engine.go();
+    }
+
+}
